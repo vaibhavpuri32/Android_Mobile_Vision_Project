16d56112598bac5e1a1db69023915e3670bbd395
vaibhavpuri32 <vaibhavpuri32@gmail.com>
2019-08-23 18:53:55 +0530

    A local Database

---
+++ lib/helper/db_helper.dart
@@ -0,0 +1,28 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqlite_api.dart';
+
+class DBHelper {
+  static Future<Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(path.join(dbPath, 'bills.db'),
+        onCreate: (db, version) {
+          return db.execute(
+              'CREATE TABLE user_bills(id TEXT PRIMARY KEY, price REAL,image TEXT, title TEXT,time TEXT ,)');
+        }, version: 1);
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    db.insert(
+      table,
+      data,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
+  }
+}
